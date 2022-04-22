674630c91550e0223bd170ae59bc7e290648eeec
Atharvaa Rane <atharvaa@Atharvaas-MacBook-Air.local>
2022-04-22 23:59:23 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+import 'pages/home_page.dart';
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-          child: Text("Hello Atharvaa :)"),
-            ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primaryColor: Colors.orange,
+          fontFamily: GoogleFonts.adamina().fontFamily,
         ),
-      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+        routes: {
+          "/": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
+        },
     );
   }
 }
